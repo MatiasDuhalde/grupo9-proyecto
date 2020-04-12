e6c786e43accf6d6d444087f8a03017b9dd124f5
Matias Duhalde <matias.duhalde@uc.cl>
2020-04-12 01:56:05 -0400

    Add rubocop stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   get 'hola', to: 'greetings#hello'
- 
-  root "greetings#hello"    #ESto es para que lo que esta en la acciòn hello dentro del controlador "greetings", salga inmediatamente de pagina de inicio
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'greetings#hello'
 end
