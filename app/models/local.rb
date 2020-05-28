5d237699f5b40683fe058d08fed0141c47265ec8
Matias Duhalde <matias.duhalde@uc.cl>
2020-05-28 16:15:26 -0400

    Add comunas-locals model

--- app/models/local.rb
+++ app/models/local.rb
@@ -4,5 +4,6 @@ class Local < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :reviews, dependent: :destroy
+  belongs_to :comuna
   has_many :matches
 end
